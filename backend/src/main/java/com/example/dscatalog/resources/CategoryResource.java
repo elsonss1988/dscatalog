a498641959bac274737e3e0a2e9e4402076c229c
Elson Santos <elsonss1988@gmail.com>
2021-10-04 15:46:53 -0300

    Find category by Id, exception handling

--- backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
@@ -1,10 +1,12 @@
 package com.example.dscatalog.resources;
 
+import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.services.CategoryServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +20,18 @@ public class CategoryResource {
     private CategoryServices categoryServices;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
+    public ResponseEntity<List<CategoryDTO>> findAll(){
         //List<Category> list = new ArrayList<>();
         //list.add(new Category(1L, "Books"));
         //list.add(new Category(2L,"Electronics"));
-        List<Category> list = new ArrayList<>();
-        list=categoryServices.findAll()
+        List<CategoryDTO> list = new ArrayList<>();
+        list=categoryServices.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<CategoryDTO> findbyId(@PathVariable Long id){
+        CategoryDTO categorydto= categoryServices.findbyId(id);
+        return ResponseEntity.ok().body(categorydto);
     }
 }
--- backend/src/main/java/com/example/dscatalog/services/CategoryServices.java
+++ backend/src/main/java/com/example/dscatalog/services/CategoryServices.java
@@ -1,18 +1,34 @@
 package com.example.dscatalog.services;
 
+import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.repository.CategoryRepository;
+import com.example.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServices {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){
+        List<Category> list=categoryRepository.findAll();
+        List<CategoryDTO> listDto= list.stream().map(x->new CategoryDTO(x)).collect(Collectors.toList());
+        return listDto;
     }
+
+    @Transactional(readOnly=true)
+    public CategoryDTO findbyId(Long id){
+        Optional <Category> category =categoryRepository.findById(id);
+        CategoryDTO dto= new CategoryDTO(category.orElseThrow(()->new EntityNotFoundException("Entity Not Found")));
+        return dto;
+    }
+
 }

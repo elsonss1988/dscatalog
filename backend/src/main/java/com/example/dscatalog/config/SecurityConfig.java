b5db4daf7e585db28f0ed6abfed1602d7bc0c865
Elson Santos <45674699+elsonss1988@users.noreply.github.com>
2022-02-24 17:15:19 -0300

    SecurityConfig

--- backend/src/main/java/com/example/dscatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/example/dscatalog/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package com.example.dscatalog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
-
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/**");
+    }
 }
